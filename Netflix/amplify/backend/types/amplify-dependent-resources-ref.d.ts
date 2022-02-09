ae194c9c73933cb7fbf60f5fa733251c9b1dfef3
Subodh22 <SubodhMaharjan33@gmail.com>
2022-02-09 12:53:02 +1100

    stuck on amplify

--- Netflix/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ Netflix/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,11 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "netflixbackend": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
     }
 }
